8c5d1100facfc4262729bf43b0a354bb14ced32d
Luca Gandini <luca.gandini88@gmail.com>
2023-04-03 18:36:15 +0200

    Dynamic message depending on percentage

--- src/App.js
+++ src/App.js
@@ -2,18 +2,42 @@ import "./App.css";
 import { getFridayLoader } from "./date-utils";
 import { getRandomIntBetweenZeroAnd } from "./math-utils";
 import useInterval from "./hooks/useInterval";
-import messages from '../resources/i18n.json';
+import messages from "./i18n.json";
 import { useCallback, useEffect, useState } from "react";
 
 const REFRESH_INTERVAL = 10000;
 
 function App() {
   const [ratio, setRatio] = useState(0);
+  const [message, setMessage] = useState("");
 
-  const computeCurrentRatio = () => {
+  const computeCurrentRatio = useCallback(() => {
     const fridayLoader = getFridayLoader();
     setRatio(fridayLoader.percentageToFridayFromToday);
+  }, []);
+
+  const getMessage = () => {
+    if (ratio < 0.5) {
+      const index = getRandomIntBetweenZeroAnd(
+        messages.weekendMessages.firstHalf.length
+      );
+      return messages.weekendMessages.firstHalf[index];
+    }
+    if (ratio < 1) {
+      const index = getRandomIntBetweenZeroAnd(
+        messages.weekendMessages.secondHalf.length
+      );
+      return messages.weekendMessages.secondHalf[index];
+    }
+    const index = getRandomIntBetweenZeroAnd(
+      messages.weekendMessages.completed.length
+    );
+    return messages.weekendMessages.completed[index];
   };
+
+  useEffect(() => {
+    setMessage(getMessage());
+  }, []);
 
   useInterval(() => {
     computeCurrentRatio();
@@ -31,29 +55,12 @@ function App() {
     }).format(ratio);
   }, [ratio]);
 
-  const getMessage = () => {
-    if (ratio < 0.5) {
-      const index = getRandomIntBetweenZeroAnd(messages['firstHalf'].length)
-      return messages.firstHalf[index];
-    }
-    if (ratio < 1) {
-      const index = getRandomIntBetweenZeroAnd(messages['secondHalf'].length)
-      return messages.secondHalf[index];
-    }
-    const index = getRandomIntBetweenZeroAnd(messages['completed'].length)
-    return messages.completed[index];
-  };
-
   return (
     <div className="App">
       <header className="App-header">
         <h1>Weekend loader</h1>
-        <label for="weekend">{ ratio === 1 ? `TGIF!` : `Friday is almost here...`}</label>
-        <progress
-          id="weekend"
-          max={1}
-          value={ratio}
-        >
+        <label htmlFor="weekend">{message}</label>
+        <progress id="weekend" max={1} value={ratio}>
           {getPercentage()}
         </progress>
         <p>{getPercentage()}</p>
--- src/math-utils.js
+++ src/math-utils.js
@@ -1,3 +1,3 @@
-export const getRandomIntBetweenZeroAnd(max) {
+export const getRandomIntBetweenZeroAnd = (max) => {
     return Math.floor(Math.random() * max);
 }
