0fba0df1d48812fccc8fb0a14072aec00ffa6d57
Luca Gandini <luca.gandini88@gmail.com>
2023-04-06 16:22:57 +0200

    test: Main page test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('GIVEN the page is rendered', () => {
+  test('THEN should be a proper title', () => {
+    render(<App />);
+    const title = screen.getByText(/Weekend loader/i);
+    expect(title).toBeInTheDocument();
+  });
+  // test('AND should be a progress bar', () => {});
+  // test('AND should be a percentage', () => {
+  //   render(<App />);
+  //   const percentage = screen.getByText(/\%/i);
+  //   expect(percentage).toBeInTheDocument();
+  // });
+  // test('AND should be a message', () => {
+  //   render(<App />);
+  //   const title = screen.getByText(/Weekend loader/i);
+  //   expect(title).toBeInTheDocument();
+  // });
 });
