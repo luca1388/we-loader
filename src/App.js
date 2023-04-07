ff7806e49067b611da03187821c06399fe018839
Luca Gandini <luca.gandini88@gmail.com>
2023-04-07 14:56:40 +0200

    refactor: Created component for message

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import "./App.css";
 import { getFridayLoader } from "./date-utils";
-import { getRandomIntBetweenZeroAnd } from "./math-utils";
 import useInterval from "./hooks/useInterval";
-import messages from "./i18n.json";
-import { useCallback, useEffect, useRef, useState } from "react";
+
+import { useCallback, useEffect, useState } from "react";
 import WinnerScreen from "./components/WinnerScreen/WinnerScreen";
 import Progress from "./components/Progress/Progress";
+import DailyMessage from "./components/DailyMessage/DailyMessage";
 
 const REFRESH_INTERVAL = 10000;
 
 function App() {
   const [ratio, setRatio] = useState(null);
-  const messageRef = useRef(null);
-  const containerRef = useRef(null);
-  const [message, setMessage] = useState("");
 
   const computeCurrentRatio = useCallback(() => {
     const fridayLoader = getFridayLoader();
@@ -21,53 +18,19 @@ function App() {
   }, []);
 
   useEffect(() => {
-    if (ratio !== null) {
-      if (messageRef.current === null) {
-        messageRef.current = true;
-      }
-    }
-  }, [ratio]);
-
-  const getMessage = useCallback(() => {
-    if (ratio < 0.5) {
-      const index = getRandomIntBetweenZeroAnd(
-        messages.weekendMessages.firstHalf.length
-      );
-      return messages.weekendMessages.firstHalf[index];
-    }
-    if (ratio < 1) {
-      const index = getRandomIntBetweenZeroAnd(
-        messages.weekendMessages.secondHalf.length
-      );
-      return messages.weekendMessages.secondHalf[index];
-    }
-    const index = getRandomIntBetweenZeroAnd(
-      messages.weekendMessages.completed.length
-    );
-    return messages.weekendMessages.completed[index];
-  }, [ratio]);
-
-  useEffect(() => {
     computeCurrentRatio();
   }, [computeCurrentRatio]);
-
-  useEffect(() => {
-    if (messageRef.current || ratio === 1) {
-      messageRef.current = false;
-      setMessage(getMessage());
-    }
-  }, [getMessage, ratio]);
 
   useInterval(() => {
     computeCurrentRatio();
   }, REFRESH_INTERVAL);
 
   return (
-    <div className="App" ref={containerRef}>
+    <div className="App">
       <header className="App-header">
         <h1 className="App-title">Weekend loader</h1>
         <Progress value={ratio} />
-        <p>{message}</p>
+        <DailyMessage ratio={ratio} />
       </header>
       {ratio === 1 && <WinnerScreen />}
     </div>
---
+++ src/components/DailyMessage/DailyMessage.js
@@ -0,0 +1,46 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+import { getRandomIntBetweenZeroAnd } from "../../math-utils";
+import messages from "../../i18n.json";
+
+const DailyMessage = ({ ratio }) => {
+  const messageRef = useRef(null);
+  const [message, setMessage] = useState("");
+
+  const getMessage = useCallback(() => {
+    if (ratio < 0.5) {
+      const index = getRandomIntBetweenZeroAnd(
+        messages.weekendMessages.firstHalf.length
+      );
+      return messages.weekendMessages.firstHalf[index];
+    }
+    if (ratio < 1) {
+      const index = getRandomIntBetweenZeroAnd(
+        messages.weekendMessages.secondHalf.length
+      );
+      return messages.weekendMessages.secondHalf[index];
+    }
+    const index = getRandomIntBetweenZeroAnd(
+      messages.weekendMessages.completed.length
+    );
+    return messages.weekendMessages.completed[index];
+  }, [ratio]);
+
+  useEffect(() => {
+    if (ratio !== null) {
+      if (messageRef.current === null) {
+        messageRef.current = true;
+      }
+    }
+  }, [ratio]);
+
+  useEffect(() => {
+    if (messageRef.current || ratio === 1) {
+      messageRef.current = false;
+      setMessage(getMessage());
+    }
+  }, [getMessage, ratio]);
+
+  return <p>{message}</p>;
+};
+
+export default DailyMessage;
